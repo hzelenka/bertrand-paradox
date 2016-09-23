9e28ac8bad136208fa447dbfa6c4db639973cbdd
Henry Zelenka <hcz2@georgetown.edu>
2016-09-23 15:46:59 -0400

    Add imports and yet-to-be-defined functions

---
+++ bertrand.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import System.Random
+import System.Environment
+
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
+
+type Radians = Double
+type CircPoint = (Double, Double)
+
+drawChord1 :: Radians -> Radians -> [(Double, Double)]
+drawChord1 endpt1 endpt2 = undefined
+
+drawChord2 :: Radians -> CircPoint -> [(Double, Double)]
+drawChord2 radius radPt = undefined
+
+drawChord3 :: Radians -> CircPoint -> [(Double, Double)]
+drawChord3 endpt midpt = undefined
+
+main :: IO ()
+main = undefined
+
