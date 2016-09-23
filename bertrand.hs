afaf07e93d3a42bd58bdff760958f6db9c23a633
Henry Zelenka <hcz2@georgetown.edu>
2016-09-23 16:55:48 -0400

    Add provisional defns of chord functions

--- bertrand.hs
+++ bertrand.hs
@@ -9,14 +9,33 @@ import Graphics.Rendering.Chart.Backend.Cairo
 type Radians = Double
 type CircPoint = (Double, Double)
 
-drawChord1 :: Radians -> Radians -> [(Double, Double)]
-drawChord1 endpt1 endpt2 = undefined
+-- Given a measurement in radians, return the coordinates of the corresponding edge point
+radToPt :: Radians -> (Double, Double)
+radToPt rad = (cos rad, sin rad)
 
-drawChord2 :: Radians -> CircPoint -> [(Double, Double)]
-drawChord2 radius radPt = undefined
+-- Distance in the Cartesian plane
+distance :: CircPoint -> CircPoint -> Double
+distance (x1, y1) (x2, y2) = sqrt $ (x2 - x1)^2 + (y2 - y1)^2
 
-drawChord3 :: Radians -> CircPoint -> [(Double, Double)]
-drawChord3 endpt midpt = undefined
+-- Method 1: Choose two random endpoints and draw a chord between them
+drawChord1 :: Radians -> Radians -> [CircPoint]
+drawChord1 endpt1 endpt2 = map radToPt [endpt1, endpt2]
+
+-- Method 2: Choose a random radius and a point on the radius, then draw a perpendicular chord
+drawChord2 :: Radians -> Double -> [CircPoint]
+drawChord2 radius radDist = [(x'+dx,y'+dy),(x'-dx,y'-dy)]
+  where (x, y)   = radToPt radius
+        (x', y') = (radDist * x, radDist * y)
+        len      = sqrt $ 1 - radDist^2 -- Pythagorean formula
+        (dx, dy) = (negate len*y, len*x)
+                           
+-- Method 3: Choose a random interior point and draw a chord through it
+drawChord3 :: CircPoint -> [CircPoint]
+drawChord3 midpt = [(x+dx,y+dy),(x-dx,y-dy)]
+  where (x, y)   = midpt
+        radDist  = distance midpt (0,0)
+        len      = sqrt $ 1 - radDist^2 -- Pythagorean formula
+        (dx, dy) = (negate len*y, len*x)
 
 main :: IO ()
 main = undefined
